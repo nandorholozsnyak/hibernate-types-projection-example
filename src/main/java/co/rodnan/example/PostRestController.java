fc00ad7ed7cc57a907bc4bebfe4085a448825a7f
Nandor Holozsnyak <nandor.holozsnyak@gmail.com>
2020-01-17 23:08:38 +0100

    - Fixed rest controller

--- src/main/java/co/rodnan/example/PostRestController.java
+++ src/main/java/co/rodnan/example/PostRestController.java
@@ -3,6 +3,7 @@ package co.rodnan.example;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +13,7 @@ public class PostRestController {
     private final PostRepository postRepository;
 
     @GetMapping(path = "/posts/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public PostDTO findById(Long id) {
+    public PostDTO findById(@PathVariable("id") Long id) {
         return postRepository.findDtoById(id);
     }
 
